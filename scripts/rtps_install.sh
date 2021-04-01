584876622e6a94bf261cc9111fcfcf3ec10c0e9f
Kevin Koffroth <ktkoffroth@gmail.com>
2021-04-01 19:39:28 -0400

    Added Install Scripts

---
+++ scripts/rtps_install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+git clone --recursive https://github.com/eProsima/Fast-DDS.git -b v2.0.0 ~/FastDDS-2.0.0
+cd ~/FastDDS-2.0.0
+mkdir build && cd build
+cmake -DTHIRDPARTY=ON -DSECURITY=ON ..
+make -j$(nproc --all)
+sudo make install
+git clone --recursive https://github.com/eProsima/Fast-DDS-Gen.git -b v1.0.4 ~/Fast-RTPS-Gen \
+    && cd ~/Fast-RTPS-Gen \
+    && ./gradlew assemble \
+    && sudo ./gradlew install
