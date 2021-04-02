1ef25042f6af85718a6af330c9967e2071a7fccf
Michael Starks <starksmichael224@gmail.com>
2021-04-01 21:09:25 -0400

    Made run script one line

--- hexacopter-env.sh
+++ hexacopter-env.sh
@@ -1,5 +1,3 @@
 #! /usr/bin/env bash
 
-WORKSPACE=$1
-
-docker run -it --rm --network=host -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/.ssh:/home/ubuntu/.ssh -v ${WORKSPACE}:/home/ubuntu/hexacopter a95245d327da bash
+docker run -it --rm --network=host -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/.ssh:/home/ubuntu/.ssh -v $1:/home/ubuntu/hexacopter a95245d327da bash
